9d7a9c5eb36e00e80c03e0df01aa39ee5d7c19d4
Matias Calvo <matiasgonzalocalvo@gmail.com>
2019-08-05 11:40:47 -0300

    add jboss and pacemaker ansible

---
+++ pacemaker/roles/OndrejHome.pcs-modules-2/library/pcs_constraint_order.py
@@ -0,0 +1,197 @@
+#!/usr/bin/python
+
+DOCUMENTATION = '''
+---
+author: "Ondrej Famera <ondrej-xa2iel8u@famera.cz>"
+module: pcs_constraint_order
+short_description: "wrapper module for 'pcs constraint order'"
+description:
+  - "module for creating and deleting clusters order constraints using 'pcs' utility"
+version_added: "2.0"
+options:
+  state:
+    description:
+      - "'present' - ensure that cluster constraint exists"
+      - "'absent' - ensure cluster constraints doesn't exist"
+    required: false
+    default: present
+    choices: ['present', 'absent']
+  resource1:
+    description:
+      - first resource for constraint
+    required: true
+  resource2:
+    description:
+      - second resource for constraint
+    required: true
+  resource1_action:
+    description:
+      - action to which constraint applies for resource1
+    required: false
+    choices: ['start','promote','demote','stop']
+    default: 'start'
+  resource2_action:
+    description:
+      - action to which constraint applies for resource2
+    required: false
+    choices: ['start','promote','demote','stop']
+    default: 'start'
+  kind:
+    description:
+      - Kind of the order constraint
+    required: false
+    choices: ['Optional','Mandatory','Serialize']
+    default: 'Mandatory'
+  symmetrical:
+    description:
+      - Is the constraint symmetrical?
+    required: false
+    choices: ['true','false']
+    default: 'true'
+  cib_file:
+    description:
+      - "Apply changes to specified file containing cluster CIB instead of running cluster."
+      - "This module requires the file to already contain cluster configuration."
+    required: false
+notes:
+   - tested on CentOS 7.3
+'''
+
+EXAMPLES = '''
+- name: start resA before starting resB
+  pcs_constraint_order: resource1='resA' resource2='resB'
+
+- name: make Optional order constraint where resA should start before resB
+  pcs_constraint_order: resource1='resA' resource2='resB' kind='Optional'
+
+- name: start resB after resA was promoted
+  pcs_constraint_order: resource1='resA' resource1_action='promote' resource2='resB'
+
+- name: start resA before starting resB but don't require that resB stope before resA
+  pcs_constraint_order: resource1='resA' resource2='resB' symmetrical='false'
+
+- name: remove order constraint between resA and resB
+  pcs_constraint_order: resource='resA' resource2='resB' state='absent'
+'''
+
+import os.path
+import xml.etree.ElementTree as ET
+from distutils.spawn import find_executable
+
+def main():
+        module = AnsibleModule(
+                argument_spec = dict(
+                        state=dict(default="present", choices=['present', 'absent']),
+                        resource1=dict(required=True),
+                        resource2=dict(required=True),
+                        resource1_action=dict(required=False, choices=['start', 'promote', 'demote', 'stop'], default='start'),
+                        resource2_action=dict(required=False, choices=['start', 'promote', 'demote', 'stop'], default='start'),
+                        kind=dict(required=False, choices=['Optional', 'Mandatory', 'Serialize'], default='Mandatory'),
+                        symmetrical=dict(required=False, choices=['true', 'false'], default='true'),
+                        cib_file=dict(required=False),
+                ),
+                supports_check_mode=True
+        )
+
+        state = module.params['state']
+        resource1 = module.params['resource1']
+        resource2 = module.params['resource2']
+        resource1_action = module.params['resource1_action']
+        resource2_action = module.params['resource2_action']
+        kind = module.params['kind']
+        symmetrical = module.params['symmetrical']
+        cib_file = module.params['cib_file']
+
+        result = {}
+
+        if find_executable('pcs') is None:
+            module.fail_json(msg="'pcs' executable not found. Install 'pcs'.")
+
+        module.params['cib_file_param'] = ''
+        if cib_file is not None:
+            ## use cib_file if specified
+            if os.path.isfile(cib_file):
+                try:
+                    current_cib = ET.parse(cib_file)
+                except Exception as e:
+                    module.fail_json(msg="Error encountered parsing the cib_file - %s" %(e) )
+                current_cib_root = current_cib.getroot()
+                module.params['cib_file_param'] = '-f ' + cib_file
+            else:
+                module.fail_json(msg="%(cib_file)s is not a file or doesn't exists" % module.params)
+        else:
+            ## get running cluster configuration
+            rc, out, err = module.run_command('pcs cluster cib')
+            if rc == 0:
+                current_cib_root = ET.fromstring(out)
+            else:
+                module.fail_json(msg='Failed to load cluster configuration', out=out, error=err)
+        
+        ## try to find the constraint we have defined
+        constraint = None
+        constraints = current_cib_root.findall("./configuration/constraints/rsc_order")
+        for constr in constraints:
+            # constraint is considered found if we see resources with same names and order as given to module
+            if constr.attrib.get('first') == resource1 and constr.attrib.get('then') == resource2:
+                constraint = constr
+                break
+
+        # additional variables for verbose output
+        result.update( {
+            'resource1_action': None if constraint is None else constraint.attrib.get('first-action'),
+            'resource2_action': None if constraint is None else constraint.attrib.get('then-action'),
+            'kind': None if constraint is None else constraint.attrib.get('kind'),
+            'symmetrical': None if constraint is None else constraint.attrib.get('symmetrical'),
+        } )
+
+        # order constraint creation command
+        cmd_create='pcs %(cib_file_param)s constraint order %(resource1_action)s %(resource1)s then %(resource2_action)s %(resource2)s kind=%(kind)s symmetrical=%(symmetrical)s' % module.params
+
+        # order constraint deletion command
+        if constraint is not None:
+            cmd_delete = 'pcs %(cib_file_param)s constraint delete '+ constraint.attrib.get('id')
+
+        if state == 'present' and constraint is None:
+            # constraint should be present, but we don't see it in configuration - lets create it
+            result['changed'] = True
+            if not module.check_mode:
+                rc, out, err = module.run_command(cmd_create)
+                if rc == 0:
+                    module.exit_json(**result)
+                else:
+                    module.fail_json(msg="Failed to create constraint with cmd: '" + cmd_create + "'", output=out, error=err)
+
+        elif state == 'present' and constraint is not None:
+            # constraint should be present based on resource names and order, lets check the attributes
+            if resource1_action != constraint.attrib.get('first-action')  or resource2_action != constraint.attrib.get('then-action') or (constraint.attrib.get('kind') is not None and constraint.attrib.get('kind') != kind) or (constraint.attrib.get('symmetrical') is not None and constraint.attrib.get('symmetrical') != symmetrical ):
+                result['changed'] = True
+                if not module.check_mode:
+                    rc, out, err = module.run_command(cmd_delete)
+                    if rc != 0:
+                        module.fail_json(msg="Failed to delete constraint for replacement with cmd: '" + cmd_delete + "'", output=out, error=err)
+                    else:
+                        rc, out, err = module.run_command(cmd_create)
+                        if rc == 0:
+                            module.exit_json(**result)
+                        else:
+                            module.fail_json(msg="Failed to create constraint replacement with cmd: '" + cmd_create + "'", output=out, error=err)
+                   
+        elif state == 'absent' and constraint is not None:
+            # constraint should not be present but we have found something - lets remove that
+            result['changed'] = True
+            if not module.check_mode:
+                rc, out, err = module.run_command(cmd_delete)
+                if rc == 0:
+                    module.exit_json(**result)
+                else:
+                    module.fail_json(msg="Failed to delete constraint with cmd: '" + cmd_delete + "'", output=out, error=err)
+        else:
+            # constraint should not be present and is not there, nothing to do
+            result['changed'] = False
+
+        ## END of module
+        module.exit_json(**result)
+
+# import module snippets
+from ansible.module_utils.basic import *
+main()
---
+++ pacemaker/roles/ondrejhome.ha-cluster-pacemaker/README.md
@@ -0,0 +1,137 @@
+ha-cluster-pacemaker
+=========
+
+Role for configuring basic pacemaker cluster on CentOS/RHEL 6/7 systems.
+
+Requirements
+------------
+
+RHEL: It is expected that machines will already be registered and subscribed for access to 'High Availability' or 'Resilient storage' channels.
+
+Role Variables
+--------------
+
+  - user used for authorizing cluster nodes
+
+    ```
+    cluster_user: hacluster
+    ```
+
+    ```
+    cluster_user_pass: testtest
+    ```
+
+  - group to which cluster user belongs (should be 'haclient')
+
+    ```
+    cluster_group: haclient
+    ```
+
+  - name of the cluster
+
+    ```
+    cluster_name: pacemaker
+    ```
+
+  - configuration of firewall for clustering, NOTE in RHEL/Centos 6 this replaces iptables configuration file!
+  
+    ```
+    cluster_firewall: true
+    ```
+
+  - enable cluster on boot
+  
+    ```
+    cluster_enable_service: true
+    ```
+
+  - configure cluster with fence_xvm fencing device ?
+    This will copy /etc/cluster/fence_xvm.key to nodes and add fencing devices to cluster
+    NOTE: you need to define 'vm_name' in the inventory for each cluster node
+  
+    ```
+    cluster_configure_fence_xvm: true
+    ```
+
+  - configure cluster with fence_kdump fencing device ?
+    This starts kdump service and defines the fence_kdump stonith devices.
+    NOTE: if the kdump service is not started this won't work properly or at all
+  
+    ```
+    cluster_configure_fence_kdump: false
+    ```
+
+  - (RHEL only) enable the repositories containint packages needed
+    ```
+    enable_repos: true
+    ```
+
+  - (RHEL only) enable the extended update (EUS) repositories containint packages needed
+    ```
+    enable_eus_repos: false
+    ```
+
+  - (RHEL only) enable the SAP Solutions update service (E4S) repositories containint packages needed
+    ```
+    enable_e4s_repos: false
+    ```
+
+  - (RHEL only) enable Beta repositories containint packages needed
+    ```
+    enable_beta_repos: false
+    ```
+
+  - (RHEL only) type of enable repositories
+    - ha - High-Availability
+    - rs - Resilient Storage
+    ```
+    repos_type: 'ha'
+    ```
+
+  - (CentOS only) install the needed packages from the CD-ROM media available at /dev/cdrom
+    ```
+    use_local_media: false
+    ```
+
+  - Enable or disable PCSD web GUI. By default the role keeps the default of installation means that
+    PCSD web GUI is disabled on CentOS/RHEL 6.X and enabled on CentOS/RHEL 7.X. `true` or `false` can
+    be passed to this variable to make sure that PCSD web GUI is enabled or disabled.
+    ```
+    enable_pcsd_gui: 'nochange'
+    ```
+
+  - Cluster transport protocol. By default this role will use what is default for give OS.
+    For CentOS/RHEL 6.X this means 'udp' (UDP multicast) and for CentOS/RHEL 7.X this means 'udpu'
+    (UDP unicast). This variable accepts following options: `default`, `udp` and `udpu`.
+    ```
+    cluster_transport: 'default'
+    ```
+
+Example Playbook
+----------------
+
+Example playbook for creating cluster named 'test1' enabled on boot, with fence_xvm and firewall settings
+
+    - hosts: servers
+      roles:
+         - { role: OndrejHome.ha-cluster-pacemaker, cluster_name: 'test1' }
+
+Example for creating cluster named 'test2' without configuring firewalling and without fence_xvm.
+For cluster to get properly authorize it is expected that firewall is already configured or disabled.
+
+    - hosts: servers
+      roles:
+         - { role: OndrejHome.ha-cluster-pacemaker, cluster_name: 'test2', cluster_firewall: false, cluster_configure_fence_xvm: false }
+
+License
+-------
+
+GPLv3
+
+Author Information
+------------------
+
+WARNING: this is alpha-version quality proof-of concept role that still needs some polishing and is using custom modules 
+         to interact with pacemaker through python. This is suitable for testing purposes only.
+
+To get in touch with author you can use email ondrej-xa2iel8u@famera.cz or create a issue on github when requesting some feature.
